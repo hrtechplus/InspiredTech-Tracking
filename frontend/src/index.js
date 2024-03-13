21fabc85394662287eb92d1574b9a7a4e017677b
Nipuna Sachintha <nipunasachintha2001@gmail.com>
2024-03-13 14:08:55 +0530

    Update index.js
    comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 
+
+//comment
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
