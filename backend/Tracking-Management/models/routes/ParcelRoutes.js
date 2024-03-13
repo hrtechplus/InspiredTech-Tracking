def8550ca07bfc356d58c97ab0159081aca05320
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-03-13 16:57:29 +0530

    Update ParcelRoutes.js

--- backend/Tracking-Management/models/routes/ParcelRoutes.js
+++ backend/Tracking-Management/models/routes/ParcelRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const parcelSchema = require("../models/parcelSchema");
-
+const express = require("express");
 const app = express();
 
 // POST endpoint to handle incoming parcel data
