d896308d43f71fc6cf19732f36e07ab89fcb269f
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-03-16 23:10:51 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import AdminLogin from "./trackingComponent/admin/AdminLogin";
-import toast, { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <div className="App">
       <AdminLogin />
-      <Toaster />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import AdminLogin from "./trackingComponent/admin/AdminLogin";
+import App from "./App";
+import toast, { Toaster } from "react-hot-toast";
 //comment
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<App />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/parcel" element={<AdminLogin />} />
       </Routes>
     </BrowserRouter>
+    <Toaster />
   </ChakraProvider>
 );
 
