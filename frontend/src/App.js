7483e4863c9743b2a4c142203958b2da6b48d7ba
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-03-16 19:34:23 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import UserTrakingSearch from "./trackingComponent/user/ParcelForm";
+import AdminLogin from "./trackingComponent/admin/AdminLogin";
+
 function App() {
   return (
     <div className="App">
-      <UserTrakingSearch />
+      <AdminLogin />
     </div>
   );
 }
