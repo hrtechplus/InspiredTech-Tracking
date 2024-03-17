3f3dab134ae119af489f4bd28a9e72436380d9c2
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-03-17 23:54:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import AdminLogin from "./trackingComponent/admin/AdminLogin";
-
+import ParcelForm from "./trackingComponent/user/ParcelForm";
 function App() {
   return (
     <div className="App">
-      <p>Home page</p>
+      <ParcelForm />
     </div>
   );
 }
