cd841fddf9db1c325d448c73e9d0e6d78f560959
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-03-20 23:56:57 +0530

    adminROutes and the savedParcel

--- backend/trackingParcel/parcelRoutes/adminRoutes.js
+++ backend/trackingParcel/parcelRoutes/adminRoutes.js
@@ -67,6 +67,40 @@ router.put("/admin/parcels/:trackingNumber", async (req, res) => {
   }
 });
 
+// POST route to handle incoming parcel data
+router.post("/admin/addparcels", async (req, res) => {
+  try {
+    // Extract parcel data from request body
+    const {
+      parcelId,
+      status,
+      handOverDate,
+      deliveryCost,
+      trackingNumber,
+      user,
+    } = req.body;
+
+    // Create a new Parcel instance with the received data
+    const newParcel = new ParcelModel({
+      parcelId,
+      status,
+      handOverDate,
+      deliveryCost,
+      trackingNumber,
+      user, // Include user reference
+      // Add more properties as needed
+    });
+
+    // Save the Parcel instance to the database
+    const savedParcel = await newParcel.save();
+
+    res.status(201).json(savedParcel); // Respond with the saved parcel data
+  } catch (error) {
+    console.error("Error saving parcel:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // DELETE parcel by tracking number
 router.delete("/admin/parcels/:trackingNumber", async (req, res) => {
   try {
--- backend/trackingParcel/parcelRoutes/saveParcelRoute.js
+++ backend/trackingParcel/parcelRoutes/saveParcelRoute.js
@@ -3,40 +3,6 @@ const router = express.Router();
 const ParcelModel = require("../models/parcelSchema"); // Update import to match the new schema filename
 const UserModel = require("../models/userSchema"); // Import User model
 
-// POST route to handle incoming parcel data
-router.post("/parcels", async (req, res) => {
-  try {
-    // Extract parcel data from request body
-    const {
-      parcelId,
-      status,
-      handOverDate,
-      deliveryCost,
-      trackingNumber,
-      user,
-    } = req.body;
-
-    // Create a new Parcel instance with the received data
-    const newParcel = new ParcelModel({
-      parcelId,
-      status,
-      handOverDate,
-      deliveryCost,
-      trackingNumber,
-      user, // Include user reference
-      // Add more properties as needed
-    });
-
-    // Save the Parcel instance to the database
-    const savedParcel = await newParcel.save();
-
-    res.status(201).json(savedParcel); // Respond with the saved parcel data
-  } catch (error) {
-    console.error("Error saving parcel:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
 // GET route to retrieve a specific parcel by tracking number
 router.get("/parcels/:trackingNumber", async (req, res) => {
   try {
