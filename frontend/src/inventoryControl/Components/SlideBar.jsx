7fe29ec30e05e4bc0b9d5f7105eaac272235b3ee
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2024-05-09 08:35:25 +0530

    Add active and inactive states to Slide bar

--- frontend/src/inventoryControl/Components/SlideBar.jsx
+++ frontend/src/inventoryControl/Components/SlideBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function SlideBar() {
+  const location = useLocation()
+
   return (
     <div className='slide-bar-container'>
         <h1 className='in-panel-slide-name'><strong>Inventory Control<br />Panel</strong></h1>
-        <div className="in-panel-slide-btn">
-            <Link to='/inventoryPanel' className='inventory-panel-home-link'>Home</Link>
+        <div className={`in-panel-slide-btn ${location.pathname === '/inventoryPanel' ? 'in-panel-slide-btn-active' : 'in-panel-slide-btn'}`}>
+            <Link to='/inventoryPanel' className={location.pathname === '/inventoryPanel' ? 'inventory-panel-home-link-active' : ''}>Home</Link>
         </div>
-        <div className="in-panel-slide-btn">
-            <Link to='/inventoryPanel/addItems' className='inventory-panel-add-item-link'>Add Items</Link>
+        <div className={`in-panel-slide-btn ${location.pathname === '/inventoryPanel/addItems' ? 'in-panel-slide-btn-active' : 'in-panel-slide-btn'}`}>
+            <Link to='/inventoryPanel/addItems' className={location.pathname === '/inventoryPanel/addItems' ? 'inventory-panel-add-item-link-active' : ''}>Add Items</Link>
         </div>
     </div>
   )
